7675b83a38807d16a47ee419fe54779506b46f31
mcordova47@gmail.com <mcordova47@gmail.com>
2018-09-25 21:07:50 -0400

    Remove query string from setRoute

--- client/src/App/Routes.purs
+++ client/src/App/Routes.purs
@@ -7,7 +7,7 @@ import Control.Alt ((<|>))
 import Control.Monad.Eff (Eff)
 import DOM (DOM)
 import DOM.HTML (window)
-import DOM.HTML.Location (reload, setHash)
+import DOM.HTML.Location (origin, pathname, reload, setHref)
 import DOM.HTML.Types (HISTORY)
 import DOM.HTML.Window (location)
 import Data.Maybe (Maybe(..), fromMaybe)
@@ -63,9 +63,11 @@ toTitle (Recipe slug) = unslugify slug <> " | Recipe Book"
 
 setRoute :: forall fx. Route -> Eff ( dom :: DOM, history :: HISTORY | fx) Unit
 setRoute route = do
-  let hash = toHash route
+  let hash = toURL route
   window' <- window
   loc <- location window'
-  setHash hash loc
+  origin' <- origin loc
+  pathname' <- pathname loc
+  setHref (origin' <> pathname' <> hash) loc
   loc' <- location window'
   reload loc'
