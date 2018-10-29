5cc7c4df91786716a6bc21ffaacb4cd7b983d761
mcordova47@gmail.com <mcordova47@gmail.com>
2018-10-28 22:10:35 -0400

    Refactor formGroups

--- client/src/App/RecipeEditor.purs
+++ client/src/App/RecipeEditor.purs
@@ -38,8 +38,30 @@ view { recipe, accessMode, onToggleEditMode } =
 
 textInput :: forall e. { value :: String, label :: String } -> HTML e
 textInput { value, label } =
+  formGroup
+    { value
+    , label
+    , input:
+        H.input
+          ! HA.className "recipe-editor__form-group__input"
+          ! HA.value value
+    }
+
+textArea :: forall e. { value :: String, label :: String } -> HTML e
+textArea { value, label } =
+  formGroup
+    { value
+    , label
+    , input:
+        H.textarea
+          ! HA.className "recipe-editor__form-group__input"
+          $ text value
+    }
+
+formGroup :: forall e. { label :: String, value :: String, input :: HTML e } -> HTML e
+formGroup { label, value, input } =
   H.div ! HA.className "recipe-editor__form-group" $ do
     H.div ! HA.className "recipe-editor__form-group__label" $ text label
-    H.input
+    input
       ! HA.className "recipe-editor__form-group__input"
       ! HA.value value
