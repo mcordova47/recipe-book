2baf31ae569d042a84c4dc77f60b5791fa07762e
mcordova47@gmail.com <mcordova47@gmail.com>
2019-03-19 00:36:17 -0400

    Update backend url

--- client/support/entry.js
+++ client/support/entry.js
@@ -1,7 +1,7 @@
 var ClientEntry = require('../src/Main.purs');
 var url = window.location.hash;
 var api = process.env.NODE_ENV === 'production'
-  ? 'https://guarded-shore-22541.herokuapp.com/api/'
+  ? 'https://recipebook-api.herokuapp.com/api/'
   : 'http://localhost:8081/api/';
 var initialState = window.__puxLastState || ClientEntry.initialState;
 var app = ClientEntry.main(url)(api)(initialState)();
